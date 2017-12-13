de3c4b355987b9d49baeee4567a9ac07153dbd0b
Artur Kudeł <akudel@apptension.com>
2017-12-13 10:28:14 +0100

    Revert changing bar canvas and bar slices

--- examples/bar-charts/src/BarChart.js
+++ examples/bar-charts/src/BarChart.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import { ResponsiveBarCanvas } from '@nivo/bar';
+import { ResponsiveBar } from '@nivo/bar';
 import { generateCountriesData } from 'nivo-generators'
 import { keys } from 'lodash';
 
 const colors = ['#fae04d', '#ff744c', '#789792', '#b1646a', '#efa9a1', '#8470c7', '#97a66f'];
 const data = generateCountriesData(['rock', 'jazz', 'hip-hop', 'reggae', 'folk'], { size: 9 });
-console.log(data);
-data[0].jazz = -77;
-data[1].jazz = -37;
 const dataKeys = keys(data[0]);
 const keyNames = dataKeys.reduce((keyNameAcc, name) => {
   keyNameAcc[name] = {
@@ -23,11 +20,11 @@ const templates = data.map(({country}) => {
 
 const Bar = () => (
   <div style={{marginTop: '80px', marginLeft: '50px', height: '400px', minWidth: '600px'}}>
-    <ResponsiveBarCanvas
+    <ResponsiveBar
       margin={{
-        top: 10,
+        top: 1.5,
         right: 0,
-        bottom: 10,
+        bottom: 2,
         left: 40,
       }}
       padding={0.2}
@@ -45,10 +42,6 @@ const Bar = () => (
       tooltipFormat={(value) => value}
       isInteractive={true}
       animate={false}
-      edge={{
-        value: 120,
-        color: '#ff2e0e'
-      }}
     />
   </div>
 )
